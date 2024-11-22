56e5665def9cef7d5c5e6a893140ad491c2e9c6b
Randimasl <117644553+Randimasl@users.noreply.github.com>
2024-11-22 17:16:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ jidNormalizedUser,
 getContentType,
 fetchLatestBaileysVersion,
 Browsers
-} = require('@whiskeysockets/baileys')
+} = require('Dark/Queen')
 
 const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson } = require('./lib/functions')
 const fs = require('fs')
