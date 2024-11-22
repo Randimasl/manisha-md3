c94c7ee2930ac27b0ce6a2edc76ce6313b86102e
Randimasl <117644553+Randimasl@users.noreply.github.com>
2024-11-22 15:19:27 +0530

    Update song.js

--- plugins/song.js
+++ plugins/song.js
@@ -9,3 +9,9 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
