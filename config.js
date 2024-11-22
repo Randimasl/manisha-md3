4c49fd9db2bce25c27ec6f70f709f538e2020306
Randimasl <117644553+Randimasl@users.noreply.github.com>
2024-11-22 14:48:30 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "WnYAmShD#yd_vK26iaD21EqW3E2p4gjYDjyOoxulC3HEiPLsvTz4",
+SESSION_ID: process.env.SESSION_ID || "DrQVyKra#WlI0J1s8379cb3q00va0ET2uFXcAVSy_nAWApcxQWlw",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/tjhxsh.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "*🎇WELLCOME TO MANISHA-MD BOT🎇*",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
