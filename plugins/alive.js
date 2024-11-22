4c32ce7cf8b2132b4f77ae9efac98111860836ae
Randimasl <117644553+Randimasl@users.noreply.github.com>
2024-11-22 16:58:59 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -4,7 +4,7 @@ const {cmd , commands} = require('../command')
 cmd({
     pattern: "alive",
     desc: "Check bot online or no.",
-    react: "👋",
+    react: "🧚‍♂",
     category: "main",
     filename: __filename
 },
