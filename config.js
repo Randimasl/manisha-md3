e4340c3908ec5ef1ecc15be3c9b5dd9636302e75
Randimasl <117644553+Randimasl@users.noreply.github.com>
2024-11-23 16:42:10 +0530

    MANISHA-MD

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "DrQVyKra#WlI0J1s8379cb3q00va0ET2uFXcAVSy_nAWApcxQWlw",
+SESSION_ID: process.env.SESSION_ID || "WnYAmShD#yd_vK26iaD21EqW3E2p4gjYDjyOoxulC3HEiPLsvTz4",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/gj8jbu.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "🧚‍♂️WELLCOME TO Dark Queen🧚‍♂️",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
