e9dbe3d7c50b4c8a2f1196ca44dd80dcb95e21f7
Randimasl <117644553+Randimasl@users.noreply.github.com>
2024-11-26 03:06:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,5 +6,5 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "PvR0EBIQ#PIjBt7lTZkUkbHI3g7IM4t2iBomLISopXqfEsP8aCPk",
-MONGODB: process.env.MONGODB || ""
+MONGODB: process.env.MONGODB || "mongodb://mongo:tmVNIICplUrrmhmGIujHVSeWeIlrbNmU@junction.proxy.rlwy.net:46680"
 };
